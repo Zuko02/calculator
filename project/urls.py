c268cd9b822f8cfdf74ec4a779a955590cecc10b
Zuko02 <zuko.audit@outlook.com>
2025-11-10 21:40:43 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from app.views import FrontendAppView
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('calculate/', include('app.urls')),  # your calculator API
     re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),  # React routes
 ]
+
