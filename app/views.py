6cb5193281e09b0e4ab8511ced591c4905b1ebb3
Zuko02 <zuko.audit@outlook.com>
2025-11-10 21:30:02 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.views.generic import View
+from django.http import FileResponse
+from django.conf import settings
+import os
 
 class CalculatorView(APIView):
     def post(self, request):
@@ -29,3 +33,11 @@ class CalculatorView(APIView):
             return Response({"error": "Invalid operation"}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({"result": result})
+
+class FrontendAppView(View):
+    def get(self, request):
+        index_file_path = os.path.join(settings.REACT_APP_DIR, 'index.html')
+        try:
+            return FileResponse(open(index_file_path, 'rb'))
+        except FileNotFoundError:
+            return FileResponse(b"Build your React app first.", status=404)
