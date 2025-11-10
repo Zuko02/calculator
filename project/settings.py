4eefe266997f440b5a5348e360c648d227b37dad
Zuko02 <zuko.audit@outlook.com>
2025-11-10 21:29:19 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -119,9 +119,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# React build directory
+REACT_APP_DIR = os.path.join(BASE_DIR, 'calculator-frontend', 'build')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'static'),
+]
+
+TEMPLATES[0]['DIRS'] = [REACT_APP_DIR]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ALLOW_ALL_ORIGINS = True
